bf7e2d3fe1e16d539c8cc402caa3e3726175bd60
pebble8888 <pebble8888@gmail.com>
2020-02-01 15:48:20 +0900

    clean

---
+++ Ripemd160/Ripemd160.h
@@ -0,0 +1,13 @@
+//
+//  Ripemd160.h
+//
+//  Copyright © 2020 pebble8888. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Ripemd160.
+FOUNDATION_EXPORT double Ripemd160VersionNumber;
+
+//! Project version string for Ripemd160.
+FOUNDATION_EXPORT const unsigned char Ripemd160VersionString[];
